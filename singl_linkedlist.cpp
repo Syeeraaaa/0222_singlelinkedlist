0241b0a88e0aff5244fc9c1f49ec62c64121c066
Syeeraaaa <syeerasilvia@gmai.com>
2025-05-06 12:23:17 +0700

    membuat void addNode

--- singl_linkedlist.cpp
+++ singl_linkedlist.cpp
@@ -22,22 +22,7 @@ public:
 
     void addNode()
     {
-        int nim;
-        cout << "\nMasukkan Nomor Mahasiswa: ";
-        cin >> nim;
-
-        Node *nodeBaru = new Node;
-        nodeBaru->noMhs = nim;
-
-        if (START == NULL || nim <= START->noMhs)
-        {
-            if ((START != NULL) && (nim == START->noMhs))
-            {
-                cout << "\nDuplikais noMhs tidak diijinkan\n";
-            }
-            nodeBaru->next = START;
-            START = nodeBaru;
-            return;
+        
         }
         Node *previous = START;
         Node *current = START;
@@ -77,7 +62,17 @@ public:
 
     bool delNode(int nim)
     {
-        
+        Node *current, *previous;
+        if (!search(nim, &previous, &current))
+            return false;
+
+        if (current == START)
+            START = START->next;
+        else
+            previous->next = current->next;
+
+        delete current;
+        return true;
     }
 
     void traverse()
